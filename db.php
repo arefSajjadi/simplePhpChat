5027af4f5728f130fd35397750cb87016085b00f
ewdmund <arefsajjadi80@gmail.com>
2021-06-06 12:56:36 +0430

    env update

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$db_username = 'root';
-$db_password = 'Aref@1574';
-$db_name = 'simple_php_chat';
-
-$con = mysqli_connect("localhost", $db_username, $db_password, $db_name);
-$con->set_charset("utf8");
