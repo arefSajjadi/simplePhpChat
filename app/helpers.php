4dc1f4044c78906ceaf89d6df9bc6fadeb6bbab5
ewdmund <arefsajjadi80@gmail.com>
2021-06-06 12:44:39 +0430

    directory refactored

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'db/db.php';
+include_once 'env.php';
 session_start();
 
 function users()
--- db/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$db_username = 'root';
-$db_password = '';
-$db_name = 'simple_php_chat';
-
-$con = mysqli_connect("localhost", $db_username, $db_password, $db_name);
-$con->set_charset("utf8");
