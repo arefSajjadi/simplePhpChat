ed47246c38127ebd8743509f76997e66e23132ac
ewdmund <arefsajjadi80@gmail.com>
2021-06-06 12:53:26 +0430

    any

--- env.php
+++ env.php
@@ -1,7 +1,7 @@
 <?php
 
 $db_username = 'root';
-$db_password = '';
+$db_password = 'Aref@1574';
 $db_name = 'simple_php_chat';
 
 $con = mysqli_connect("localhost", $db_username, $db_password, $db_name);
